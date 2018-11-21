3b158c699f4947ce7088632b9e1c2110988dba87
Raythan <33856352+Raythan@users.noreply.github.com>
2018-11-21 20:31:20 -0200

    Add files via upload

--- _js/main.js
+++ _js/main.js
@@ -1,19 +1,24 @@
 function alertPassword(){
 	var inputAlert = document.getElementById("inputErro").value,
 	inputAlert1 = document.getElementById("inputErro1").value,
-	spanAlert = document.getElementById("spanErro");
-	if(inputAlert == ""){
-		spanAlert.innerHTML = "Valor não pode ser vazio.<br/>";
-	}else{
-		if(inputAlert!=inputAlert1){
-			spanAlert.innerHTML = "Esses números não são iguais.<br/>";
+	spanAlert = document.getElementById("spanErro"),
+	spanAlert1 = document.getElementById("spanErro1");
+	
+	
+	
+	if(inputAlert===inputAlert1){
+		if(inputAlert.length<8){
+			spanAlert.innerHTML = "A senha precisa ter 8 caracteres.<br/>";
 		}else{
-			if(inputAlert.length<8){
-				spanAlert.innerHTML = "A senha precisa ter 8 caracteres.<br/>";
-			}else{
-				spanAlert.innerHTML = "";
-			}
+			spanAlert.innerHTML = "";
 		}
+		if(inputAlert1.length<8){
+			spanAlert1.innerHTML = "A senha precisa ter 8 caracteres.<br/>";
+		}else{
+			spanAlert1.innerHTML = "";
+		}
+	}else{
+		spanAlert.innerHTML = "Esses números não são iguais.<br/>";
 	}
 	
 	
